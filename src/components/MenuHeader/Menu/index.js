af1d12526419863c6962b03dcc208b2118afb376
Konstantin Korostelev <korostel97@gmail.com>
2021-03-17 07:35:44 +0300

    routes is array now

--- src/components/MenuHeader/Menu/index.js
+++ src/components/MenuHeader/Menu/index.js
@@ -2,31 +2,27 @@ import cn from 'classnames';
 import s from './style.module.css';
 
 const Menu = ({ isActive }) => {
+  const ROUTES = [
+    { name: 'HOME', href: '#welcome', key: 1 },
+    { name: 'GAME', href: '#game', key: 2 },
+    { name: 'ABOUT', href: '#about', key: 3 },
+    { name: 'CONTACT', href: '#contact', key: 4 }
+  ];
+
   return (
     <div className={ cn(s.menuContainer, { [s.active]: isActive, [s.diactive]: !isActive }) }>
       <div className={ cn(s.overlay) }/>
       <div className={ cn(s.menuItems) }>
         <ul>
-          <li>
-            <a href="#welcome">
-              HOME
-            </a>
-          </li>
-          <li>
-            <a href="#game">
-              GAME
-            </a>
-          </li>
-          <li>
-            <a href="#about">
-              ABOUT
-            </a>
-          </li>
-          <li>
-            <a href="#contact">
-              CONTACT
-            </a>
-          </li>
+	        { 
+            ROUTES.map(rout => <li  
+                key={rout.key}
+              >
+              <a href={rout.href}>
+                {rout.name}
+              </a>
+            </li>)
+          }
         </ul>
       </div>
     </div>
