e830bfb4a824270ad16b9393580b6cbb02ebfb98
Konstantin Korostelev <korostel97@gmail.com>
2021-03-17 20:49:40 +0300

    fix close menu

--- src/components/MenuHeader/Menu/index.js
+++ src/components/MenuHeader/Menu/index.js
@@ -1,28 +1,30 @@
 import cn from 'classnames';
 import s from './style.module.css';
 
+const MENU = [
+  { name: 'HOME', href: '#welcome', key: 1 },
+  { name: 'GAME', href: '#game', key: 2 },
+  { name: 'ABOUT', href: '#about', key: 3 },
+  { name: 'CONTACT', href: '#contact', key: 4 }
+];
+
 const Menu = ({ isOpen }) => {
-  const ROUTES = [
-    { name: 'HOME', href: '#welcome', key: 1 },
-    { name: 'GAME', href: '#game', key: 2 },
-    { name: 'ABOUT', href: '#about', key: 3 },
-    { name: 'CONTACT', href: '#contact', key: 4 }
-  ];
+  
 
   return (
     <div className={ cn(s.menuContainer, {
          [s.active]: isOpen === true,
-         [s.diactive]: isOpen === false 
+         [s.deactive]: isOpen === false 
      })}>
       <div className={ cn(s.overlay) }/>
       <div className={ cn(s.menuItems) }>
         <ul>
 	        { 
-            ROUTES.map(rout => <li  
-                key={rout.key}
+            MENU.map(item => <li  
+                key={item.key}
               >
-              <a href={rout.href}>
-                {rout.name}
+              <a href={item.href}>
+                {item.name}
               </a>
             </li>)
           }
